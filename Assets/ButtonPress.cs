fe066f2c09638317b6367c1fe2246ccb17676303
nickburggraaff <nick.burggraaff@gmail.com>
2020-01-31 20:45:30 +0100

    End button

---
+++ Assets/ButtonPress.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    public Collider collider;
+    private Animation _animation;
+
+    private void Awake()
+    {
+        _animation = GetComponent<Animation>();
+        if (_animation == null) Debug.Log("No animation found");
+        if (collider == null) Debug.Log("No collider specified");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == collider)
+        {
+            _animation.Play();
+            // TODO: Some kind of trigger to end the game
+        }
+    }
+}
