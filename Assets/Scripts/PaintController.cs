4b09bbd3698cdd975e468c3aa312602b0485cc45
M_Dreyar <stoeremitch@hotmail.com>
2020-01-31 23:20:53 +0100

    Add brushSize and brushColor to paintController

--- Assets/Scripts/PaintController.cs
+++ Assets/Scripts/PaintController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class PaintController : MonoBehaviour
 {
+    public int brushSize = 20;
+    public Color paintColor = Color.red;
+
     private SpriteRenderer spriteRenderer;
     private Texture2D texture;
-    private Color[] paint;
 
     // Start is called before the first frame update
     void Start()
@@ -21,33 +23,29 @@ public class PaintController : MonoBehaviour
         MaterialPropertyBlock block = new MaterialPropertyBlock();
         block.SetTexture("_MainTex", texture);
         spriteRenderer.SetPropertyBlock(block);
-
-        paint = Enumerable.Repeat(Color.red, 10 * 10).ToArray();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {  
-        
     }
 
     private void OnTriggerStay(Collider other)
     {
+        // Calculate where the collider hit in this objects local space
         Vector3 hitPoint = transform.InverseTransformPoint(other.gameObject.transform.position);
-        Debug.Log("Hit at: " + hitPoint);
+        //Debug.Log("Hit at: " + hitPoint);
 
+        // Move this objects local space origin to the bottom left corner to match the Texture2D origin
         Vector3 halfSpriteSizeLocalSpace = spriteRenderer.bounds.size / 2;
-
         Vector3 hitPointPixelSpace = hitPoint + halfSpriteSizeLocalSpace;
-        Debug.Log("Hit in pixel space: " + hitPointPixelSpace);
+        //Debug.Log("Hit in pixel space: " + hitPointPixelSpace);
 
+        // Scale the hit from local size to pixel size
         float scaleX = (spriteRenderer.bounds.size.x) / texture.width;
         float scaleY = (spriteRenderer.bounds.size.y) / texture.height;
-
         hitPointPixelSpace.x /= scaleX;
         hitPointPixelSpace.y /= scaleY;
+        //Debug.Log(hitPointPixelSpace);
 
-        texture.SetPixels((int)hitPointPixelSpace.x, (int)hitPointPixelSpace.y, 10, 10, paint);
+        // Change the color of the pixels to the paint color
+        Color[] paint = Enumerable.Repeat(paintColor, brushSize * brushSize).ToArray();
+        texture.SetPixels((int)Mathf.Clamp(hitPointPixelSpace.x - (brushSize / 2), 0, texture.width - brushSize), (int)Mathf.Clamp(hitPointPixelSpace.y - (brushSize / 2), 0, texture.height - brushSize), brushSize, brushSize, paint, 0);
         texture.Apply();
     }
 }
