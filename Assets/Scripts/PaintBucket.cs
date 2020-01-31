44b12b9ec51a6c38a8674b7b2cf28f80ab279647
nickburggraaff <nick.burggraaff@gmail.com>
2020-01-31 21:47:58 +0100

    PaintBrush.cs PaintBucket.cs
    When they touch they change color!

---
+++ Assets/Scripts/PaintBucket.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintBucket : MonoBehaviour
+{
+    public Color paintbucketColor;
+    private Renderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+        if (_renderer != null) _renderer.material.color = paintbucketColor;
+    }
+
+    public void ChangePaintOnBrush(PaintBrush brush)
+    {
+        brush.color = paintbucketColor;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PaintBrush>() != null)
+        {
+            PaintBrush brush = other.gameObject.GetComponent<PaintBrush>();
+            ChangePaintOnBrush(brush);
+        }
+    }
+}
