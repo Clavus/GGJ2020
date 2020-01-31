25da3c5358c3ac7e76eed3ae5bca4d6bb54f378d
Clavus <joerivdvelden@gmail.com>
2020-01-31 20:48:39 +0100

    Set up Oculus avatar. Game script set up.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Game : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject vrRig;
+	[SerializeField]
+	private GameObject noVrRig;
+	[SerializeField]
+	private bool forceStartNoVr;
+
+	private void Start()
+	{
+		bool useVR = XRSettings.enabled && !forceStartNoVr;
+		vrRig.SetActive(useVR);
+		noVrRig.SetActive(!useVR);
+		if (!useVR)
+			XRSettings.enabled = false;
+	}
+}
