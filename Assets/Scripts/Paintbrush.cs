cb34f4aaa22536c2cae3914a8da74960cb4563bb
Clavus <joerivdvelden@gmail.com>
2020-01-31 22:35:31 +0100

    Changes to interactable stuff again

--- Assets/Scripts/Paintbrush.cs
+++ Assets/Scripts/Paintbrush.cs
@@ -6,12 +6,16 @@ public class PaintBrush : MonoBehaviour, IInteractable
 {
 	public Color color;
 
-	public Rigidbody Body => GetComponent<Rigidbody>();
+	public Transform GrabTransform => transform;
 	public bool CanGrab => true;
 	public bool CanInteract => true;
 
 	public void Grab(IInteracter interacter)
 	{
+		var body = GetComponent<Rigidbody>();
+		transform.localPosition = Vector3.zero;
+		transform.localRotation = Quaternion.identity;
+		body.useGravity = false;
 		interacter.Attach(this);
 	}
 
@@ -22,8 +26,9 @@ public class PaintBrush : MonoBehaviour, IInteractable
 
 	public void StopGrab()
 	{
-		Body.useGravity = true;
-		Body.transform.SetParent(null);
+		var body = GetComponent<Rigidbody>();
+		body.useGravity = true;
+		transform.SetParent(null);
 	}
 
 	public void StopInteract()
