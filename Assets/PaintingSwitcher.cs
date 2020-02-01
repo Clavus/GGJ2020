0d091b19e4841ddacef3c72535c21e8403e81334
Clavus <joerivdvelden@gmail.com>
2020-02-01 11:18:57 +0100

    Scenario system. Added "Lying cat" scenario stuff. Fixed some weird setup in countdown canvas.

--- Assets/PaintingSwitcher.cs
+++ Assets/PaintingSwitcher.cs
@@ -5,34 +5,36 @@ using UnityEngine;
 
 public class PaintingSwitcher : MonoBehaviour
 {
-    public GameObject PaintCanvasObject;
-    public Material[] Paintings;
-    private Material[] _paintings;
+	public GameObject PaintCanvasObject;
+	public Material[] Paintings;
+	private Material[] _paintings;
 
-    private void Awake()
-    {
-        _paintings = (Material[])Paintings.Clone();
-    }
+	private void Awake()
+	{
+		_paintings = (Material[])Paintings.Clone();
+	}
 
-    public void SwitchPainting()
-    {
-        var currentMaterial = PaintCanvasObject.GetComponent<MeshRenderer>().material;
-        int index = UnityEngine.Random.Range(0, Paintings.Length);
-        bool quitLoop = false;
-        int nullCount = 0;
-        while (_paintings[index] == null || currentMaterial.mainTexture == _paintings[index].mainTexture)
-        {
-            index = UnityEngine.Random.Range(0, _paintings.Length);
-            for(int i = 0; i < _paintings.Length; i++) if (_paintings[i] == null) nullCount++;
-            if (nullCount == _paintings.Length)
-            {
-                _paintings = (Material[])Paintings.Clone();
-                quitLoop = true;
-            }
-        }
+	public void SwitchPainting()
+	{
+		var currentMaterial = PaintCanvasObject.GetComponent<MeshRenderer>().material;
+		int index = UnityEngine.Random.Range(0, Paintings.Length);
+		bool quitLoop = false;
+		int nullCount = 0;
+		while (_paintings[index] == null || currentMaterial.mainTexture == _paintings[index].mainTexture)
+		{
+			index = UnityEngine.Random.Range(0, _paintings.Length);
+			for (int i = 0; i < _paintings.Length; i++)
+				if (_paintings[i] == null)
+					nullCount++;
+			if (nullCount == _paintings.Length)
+			{
+				_paintings = (Material[])Paintings.Clone();
+				quitLoop = true;
+			}
+		}
 
-        PaintCanvasObject.GetComponent<MeshRenderer>().material = _paintings[index];
-        _paintings[index] = null;
-        PaintCanvasObject.GetComponent<PaintController>().UpdateTexture();
-    }
+		PaintCanvasObject.GetComponent<MeshRenderer>().material = _paintings[index];
+		_paintings[index] = null;
+		//PaintCanvasObject.GetComponent<PaintController>().UpdateTexture();
+	}
 }
